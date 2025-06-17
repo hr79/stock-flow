ee255d53483c745bbafa37f9c5f7ee7646db0493
hr79 <doox2oo4114@gmail.com>
2025-06-17 18:45:19 +0900

    [feature]
    - 각 도메인의 repository 생성

---
+++ src/main/java/com/example/stockflow/domain/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.stockflow.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+    List<Product> findProductsByNameIn(List<String> names);
+}
