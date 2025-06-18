9d50db34a45b39c7257fb78cc76b1988bce6dc13
hr79 <doox2oo4114@gmail.com>
2025-06-19 03:59:24 +0900

    [feature]
    - WEBHOOK_URL 수정
    - notification 기능 추가(discord)

---
+++ src/main/java/com/example/stockflow/notification/DiscordNotifier.java
@@ -0,0 +1,40 @@
+package com.example.stockflow.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component("discordNotifier")
+@Slf4j
+public class DiscordNotifier implements Notifier {
+    @Value("${notifier.discord}") private String WEBHOOK_URL; // https://discord.com/api/webhooks/~~/~~ 형식
+
+    private final WebClient webClient;
+
+    public DiscordNotifier() {
+        this.webClient = WebClient.create();
+    }
+
+    @Override
+    public void notify(String message) {
+        log.info(":::: DiscordNotifier");
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("content", message);
+
+        webClient.post()
+                .uri(WEBHOOK_URL)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .bodyValue(payload)
+                .retrieve()
+                .bodyToMono(String.class)
+                .subscribe(
+                        success -> log.info("입고 요청 알림 전송 성공"),
+                        error -> log.warn("입고 요청 알림 전송 실패", error)
+                );
+    }
+}
---
+++ src/main/java/com/example/stockflow/notification/Notifier.java
@@ -0,0 +1,5 @@
+package com.example.stockflow.notification;
+
+public interface Notifier {
+    void notify(String message);
+}
