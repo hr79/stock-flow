0e1aa39c4ec77dc5e9527aa195da1ee2227f70ac
hr79 <doox2oo4114@gmail.com>
2025-06-19 03:29:55 +0900

    [feature]
    - notification 기능 추가(discord)

---
+++ src/main/java/com/example/stockflow/notification/EmailNotifier.java
@@ -0,0 +1,12 @@
+package com.example.stockflow.notification;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailNotifier implements Notifier {
+
+    @Override
+    public void notify(String message) {
+
+    }
+}
